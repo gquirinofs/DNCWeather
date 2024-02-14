c2719400251b4b21cce771e598a4135486025651
Guilherme Quirino <profguilhermequirino@gmail.com>
2024-02-14 16:35:31 -0300

    API Impl. | 70%

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,22 @@
-async function getAddressByCep() {
+async function getAddressByCep() { // Consulta o CEP e apresenta o resultado
     const cep = document.querySelector('#cepValue').value;
     try {
         const response = await fetch (`https://viacep.com.br/ws/${cep}/json/`);
         const data = await response.json();
         console.log(data);
-        const rua = document.querySelector('#rua').value;
-        rua = data.logradouro;
-        
+        const rua = data.logradouro;
+        const bairro = data.bairro;
+        const localidade = `${data.localidade}/${data.uf}`;
+        document.querySelector('#rua').innerHTML = rua;
+        document.querySelector('#bairro').innerHTML = bairro;
+        document.querySelector('#uf').innerHTML = localidade;
     } catch (error) {
-        alert(error.message);
+        alert("O CEP não deve conter: \n\n 1. Caracteres especiais; \n 2. Espaço entre os números; \n\n Tente novamente! ");
     }
 }
+
+// Previnir a atualização da página
+
+document.querySelector('#subButton').addEventListener("click", function(e){
+    e.preventDefault()
+});
